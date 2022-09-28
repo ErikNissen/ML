9086b75a68499959f29ec993e73d0c2c57a91834
nissene99 <enissenacc@protonmail.com>
2022-09-28 14:20:48 +0200

    init

---
+++ main.py
@@ -0,0 +1,24 @@
+import math, csv
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+def main():
+	coords = []
+	data = open('data.csv', newline='')
+	reader = csv.reader(data)
+	for row in reader:
+		row = row[0].split(';')
+		if row[0] == 'x':
+			continue
+		coords.append((float(row[0]), float(row[1])))
+	data.close()
+	
+	# plot the data
+	plt.plot([x for x, y in coords], [y for x, y in coords], 'ro')
+	plt.show()
+	
+	
+	
+if __name__ == '__main__':
+    	main()
