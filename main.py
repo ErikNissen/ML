e40d956992ea1288fd5249d6e6ba9ada6514367b
nissene99 <enissenacc@protonmail.com>
2022-09-28 14:36:07 +0200

    update

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import math, csv
 import matplotlib
+
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+
+M = 0
+SW = 0
+Genauigkeit = 0.05
+Q_Fehler = []
+DIFF = []
+
+
+	
 
 def main():
 	coords = []
@@ -13,12 +23,12 @@ def main():
 			continue
 		coords.append((float(row[0]), float(row[1])))
 	data.close()
-	
+
 	# plot the data
+	plt.plot([x for x, y in coords], [y for x, y in coords], 'b-')
 	plt.plot([x for x, y in coords], [y for x, y in coords], 'ro')
 	plt.show()
-	
-	
-	
+
+
 if __name__ == '__main__':
-    	main()
+	main()
